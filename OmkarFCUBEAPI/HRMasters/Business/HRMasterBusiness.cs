c904c0b914650acba81d6be9fd15ffb926b06a7f
aashishwarenew <aashishware@meritinfo.com>
2023-05-02 15:51:31 +0530

    HR Master added

---
+++ OmkarFCUBEAPI/HRMasters/Business/HRMasterBusiness.cs
@@ -0,0 +1,23 @@
+﻿using HRMasters.Models;
+using HRMasters.Repository;
+
+namespace HRMasters.Business
+{
+    public class HRMasterBusiness : IHRMasterBusiness
+    {
+        readonly IHRMasterRepository hrMasterRepository;
+        public HRMasterBusiness(IHRMasterRepository _hrMasterRepository)
+        {
+            hrMasterRepository = _hrMasterRepository;
+        }
+
+        /// <summary>
+        /// Business method for save product group master details
+        /// </summary>
+        /// <param name="productGroupMasterModel"></param>
+        public async Task<ResponseModel> HRMasterSave(HRMasterModel hrMasterModel)
+        {
+            return await hrMasterRepository.HRMasterSave(hrMasterModel);
+        }
+    }
+}
