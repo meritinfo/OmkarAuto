5dbf37f42de2a978fb0e317c5c6bacfef433aef3
Milan Sahu <milansahu000@gmail.com>
2023-11-30 15:35:10 +0530

    Updated filter condition

--- OmkarFCUBEAPP/src/app/module/tripsheet/tripsheetlist/tripsheetlist.component.ts
+++ OmkarFCUBEAPP/src/app/module/tripsheet/tripsheetlist/tripsheetlist.component.ts
@@ -194,7 +194,7 @@ export class TripsheetlistComponent {
     this.filter.fromDate = this.formFilter.value.fromDate;
     this.filter.toDate = this.formFilter.value.toDate;
     this.filter.branch = this.formFilter.value.branch === '0' ? '' : this.formFilter.value.branch;
-    this.filter.vehicle = this.formFilter.value.vehicle.dataId;
+    this.filter.vehicle = this.formFilter.value.vehicle === "" ? '' : this.formFilter.value.vehicle.dataId;
     this.tripSheetService.getTripSheetList(this.filter)
       .subscribe(resp => {
         this.allTripSheetTypes = resp;
