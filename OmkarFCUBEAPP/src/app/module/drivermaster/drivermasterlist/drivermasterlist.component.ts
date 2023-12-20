c1366967d4d00f7f0bf38d240ee902cc728aef38
Milan Sahu <milansahu000@gmail.com>
2023-12-21 00:25:08 +0530

    Updated privilage condition

--- OmkarFCUBEAPP/src/app/module/drivermaster/drivermasterlist/drivermasterlist.component.ts
+++ OmkarFCUBEAPP/src/app/module/drivermaster/drivermasterlist/drivermasterlist.component.ts
@@ -16,7 +16,7 @@ import { DataTableDirective } from 'angular-datatables';
   styleUrls: ['./drivermasterlist.component.css']
 })
 export class DrivermasterlistComponent {
-  
+
   createStatus = false;
   editStatus = false;
   deleteStatus = false;
@@ -37,17 +37,16 @@ export class DrivermasterlistComponent {
 
   formFilter!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private drivermasterService: DrivermasterService, 
+  constructor(private formBuilder: FormBuilder, private drivermasterService: DrivermasterService,
     private route: Router, private excelService: ExcelService, private pdfService: PdfService) {
   }
 
   ngOnInit(): void {
-    
+
     var menuData = sessionStorage.getItem('menulist')?.toString();
     if (typeof menuData !== 'undefined' && menuData !== null && menuData !== '') {
       var privilegeData = JSON.parse(menuData);
-      var privilegeStatus = privilegeData.find((item: { menuList: any; }) => item.menuList)
-      .menuList.find((aa: { menuName: string; }) => aa.menuName === "Driver Master");
+      const privilegeStatus = privilegeData.flatMap((item: { menuList: any; }) => item.menuList).find((( aa: { menuName: string; }) => aa.menuName === "Driver Master"));
       if (privilegeStatus) {
         this.createStatus = privilegeStatus.createYN.toLowerCase() === "y" ? true : false;
         this.editStatus = privilegeStatus.editYN.toLowerCase() === "y" ? true : false;
