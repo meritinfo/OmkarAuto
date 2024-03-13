a6759f95e19b2545a99c569454f665e6bc727ad3
Milan Sahu <milansahu000@gmail.com>
2024-03-13 22:30:53 +0530

    Idle popup implimented

--- OmkarFCUBEAPP/src/app/app.component.ts
+++ OmkarFCUBEAPP/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { timer } from 'rxjs';
 import { Router } from '@angular/router';
 import { Loginmodel } from './models/loginmodel';
 import { LoggedinUsermodel } from './models/loggedinusermodel';
+import { DEFAULT_INTERRUPTSOURCES, Idle } from '@ng-idle/core';
+import { Keepalive } from '@ng-idle/keepalive';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +18,12 @@ export class AppComponent implements OnInit {
   showModalBox: boolean = false;
   user: string = '';
 
-  constructor(public sharedService: SharedService, private route: Router, private loginModel: Loginmodel) {
+  idleState = 'Not started.';
+  timedOut = false;
+  lastPing?: Date;
+
+  constructor(public sharedService: SharedService, private route: Router, private loginModel: Loginmodel, private idle: Idle, private keepalive: Keepalive) {
+
   }
 
   ngOnInit(): void {
@@ -29,12 +36,51 @@ export class AppComponent implements OnInit {
       this.user = userName;
     }
 
-    // starts immediately, then every 20 minutes
-    timer(1200000, 1200000).subscribe(() => {
-      if (this.sharedService.loggedInStatus) {
+    if (this.sharedService.loggedInStatus) {
+      // sets an idle timeout of 900 seconds.
+      this.idle.setIdle(900);
+      // sets a timeout period of 30 seconds. after 10 seconds of inactivity, the user will be considered timed out.
+      this.idle.setTimeout(30);
+      // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
+      this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+
+      this.idle.onIdleEnd.subscribe(() => {
+        this.idleState = 'No longer idle.'
+        this.reset();
+      });
+
+      this.idle.onTimeout.subscribe(() => {
+        this.idleState = 'Timed out!';
+        this.timedOut = true;
+        this.showModalBox = false;
+        this.sharedService.loggedInStatus = false;
+        localStorage.clear();
+        sessionStorage.clear();
+        this.route.navigate(['']);
+      });
+
+      this.idle.onIdleStart.subscribe(() => {
+        this.idleState = 'You\'ve gone idle!'
         this.showModalBox = true;
-      }
-    });
+      });
+
+      this.idle.onTimeoutWarning.subscribe((countdown) => {
+        this.idleState = 'You will time out in ' + countdown + ' seconds!'
+      });
+
+      // sets the ping interval to 15 seconds
+      this.keepalive.interval(15);
+
+      this.keepalive.onPing.subscribe(() => this.lastPing = new Date());
+
+      this.reset();
+    }
+  }
+
+  reset() {
+    this.idle.watch();
+    this.idleState = 'Started';
+    this.timedOut = false;
   }
 
   //Logout from system
@@ -47,6 +93,7 @@ export class AppComponent implements OnInit {
   }
 
   stay() {
+    this.reset();
     this.showModalBox = false;
     this.loginModel.userName = this.user;
     this.sharedService.refreshToken(this.loginModel).subscribe((res: LoggedinUsermodel) => {
