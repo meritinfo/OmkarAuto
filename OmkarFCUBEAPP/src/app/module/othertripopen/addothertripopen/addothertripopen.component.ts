55762fd3370ef84428975c2e8682c4ebbfa4317d
aashishwarenew <aashishware@meritinfo.com>
2024-03-18 14:59:48 +0530

    other trip

--- OmkarFCUBEAPP/src/app/module/othertripopen/addothertripopen/addothertripopen.component.ts
+++ OmkarFCUBEAPP/src/app/module/othertripopen/addothertripopen/addothertripopen.component.ts
@@ -27,6 +27,7 @@ export class AddothertripopenComponent {
   loggedInUserID: string = '';
   year: string = '';
   loginDate: string = '';
+  branch: string = '';
   tripkms: string = '';
   advancePay: string = '';
   dTripKM_1: number = 0;
@@ -116,8 +117,14 @@ export class AddothertripopenComponent {
     else {
       this.route.navigate(['/']);
     }
+    var userData3 = sessionStorage.getItem('userBranch')?.toString();
+    if (typeof userData3 !== 'undefined' && userData3 !== null && userData3 !== '') {
+      this.branch = userData3;
+
+    }
+
     this.formOtherTripOpen = this.formBuilder.group({
-      tripBranch: new FormControl('',[Validators.required]),
+      tripBranch: new FormControl(this.branch,[Validators.required]),
       vehicleMasterID: new FormControl('',[Validators.required]),
       tripNo: new FormControl('',[Validators.required]),
       newTripDate: new FormControl('',[Validators.required]),
@@ -165,14 +172,14 @@ export class AddothertripopenComponent {
           driverMasterID: this.driverList.find(e => e.dataId == this.selectedTripSheetDetails.driverMasterID),
           contents: this.contentList.find(e => e.dataId == this.selectedTripSheetDetails.contents),
           yearid: this.year
-        });
-      } 
-      this.editMode = true;
-      this.formOtherTripOpen.controls['tripBranch'].disable();
-      this.formOtherTripOpen.controls['vehicleMasterID'].disable();
-      this.formOtherTripOpen.controls['compNonCompStatus'].disable();       
-      this.formOtherTripOpen.controls['challanNo'].disable();     
+        });        
+        this.editMode = true;
+        this.formOtherTripOpen.controls['tripBranch'].disable();
+        this.formOtherTripOpen.controls['vehicleMasterID'].disable();
+        this.formOtherTripOpen.controls['compNonCompStatus'].disable();       
+        this.formOtherTripOpen.controls['challanNo'].disable();    
 
+      } 
     }, 2000);
     
     this.formOtherTripOpen.controls['tripNo'].disable();
