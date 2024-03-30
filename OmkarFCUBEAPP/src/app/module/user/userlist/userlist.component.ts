8fbe4ebb3830d311c8b86226e56e7f89b734dd4f
Puttareddy-Suneetha <puttareddy.sunitha@meritinfo.com>
2024-03-30 16:45:10 +0530

    gst feedback

--- OmkarFCUBEAPP/src/app/module/user/userlist/userlist.component.ts
+++ OmkarFCUBEAPP/src/app/module/user/userlist/userlist.component.ts
@@ -51,6 +51,7 @@ export class UserlistComponent implements OnInit {
       pageLength: 10,
       serverSide: true,
       processing: true,
+      searching:false,
       ajax: (dataTablesParameters: any, callback) => {
         // Filter setting
         this.filter.pageNumber = (dataTablesParameters.start / dataTablesParameters.length) + 1;
