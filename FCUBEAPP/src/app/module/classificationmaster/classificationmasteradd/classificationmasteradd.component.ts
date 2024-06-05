238cd8827992f942e76642db2ab39ebb23b108c3
Puttareddy-Suneetha <puttareddy.sunitha@meritinfo.com>
2024-06-05 13:48:51 +0530

    classification

--- FCUBEAPP/src/app/module/classificationmaster/classificationmasteradd/classificationmasteradd.component.ts
+++ FCUBEAPP/src/app/module/classificationmaster/classificationmasteradd/classificationmasteradd.component.ts
@@ -67,9 +67,7 @@ export class ClassificationmasteraddComponent {
       this.route.navigate(['/']);
     }
 
-    this.sharedService.loading = true;
-
-   
+    this.sharedService.loading = true;   
 
     this.selectedClassificationMasterDetails = this.classificationmasterService.getClassificationMasterDetails();
     this.formClassificationMaster = this.formBuilder.group({   
@@ -89,9 +87,7 @@ export class ClassificationmasteraddComponent {
   // convenience getter for easy access to contact form fields
   get f() { return this.formClassificationMaster.controls; }
 
-  //Get Classification List details //
-  
-  
+  //Get Classification List details //  
   
 
   // chkClassificationNameExits(e: any) { 
@@ -112,30 +108,11 @@ export class ClassificationmasteraddComponent {
   //   }
   // }
 
-  // deleteClassificationMasterForm(): void {
-  //   if(this.selectedClassificationMasterDetails.centreid != '' ){      
-  //     this.sharedService.loading = true;
-  //     this.requestmodel.strRequest =this.selectedClassificationMasterDetails.centreid
-  //     if (confirm("Are you sure, you want to delete this?")) {
-  //           this.classificationmasterService.classificationMasterDetailsDelete(this.requestmodel).subscribe((res: Responsemodel) => {
-  //           this.responseDetails = res;
-  //           if (this.responseDetails.status) {
-  //             this.toasterService.success(this.responseDetails.message);
-  //             this.formClassificationMaster.reset();
-  //             this.route.navigate(['/classificationmasterlist']);
-  //           }
-  //           else {
-  //             this.toasterService.warning(this.responseDetails.message);
-  //           }    
-  //       });
-  //     }      
-  //     this.sharedService.loading = false;
-  //   }
-  // }
   exit(): void {
     this.route.navigate(['/classmasterlist']);
   }
-  classificationMasterDelete(): void {
+
+  deleteClassificationMasterForm(): void {
     if(this.selectedClassificationMasterDetails.classId != '' ){
      this.requestmodel.strRequest =this.selectedClassificationMasterDetails.classId
       if (confirm("Are you sure, you want to delete this?")) {
@@ -173,14 +150,9 @@ export class ClassificationmasteraddComponent {
     var selectedDataVal = this.formClassificationMaster.getRawValue();
     this.userSubmitted = true;
     this.classificationModel.classId = this.selectedClassificationMasterDetails.classId ;
-    this.classificationModel.classDesc             = selectedDataVal.classDesc.toString().toUpperCase();
-    this.classificationModel.isActive       = selectedDataVal.isActive;
-
-
-
-
+    this.classificationModel.classDesc  = selectedDataVal.classDesc.toString().toUpperCase();
+    this.classificationModel.isActive = selectedDataVal.isActive;
     //this.classificationModel.loggedInUserID   = this.loggedInUserID;
-
   
     this.classificationmasterService.classificationmasterSubmitted(this.classificationModel).subscribe((res: Responsemodel) => {
       this.responseDetails = res;
