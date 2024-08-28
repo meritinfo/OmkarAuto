ed6b6b36eb577c467a0e35e9bea55138b7706638
Puttareddy-Suneetha <puttareddy.sunitha@meritinfo.com>
2024-08-28 16:10:08 +0530

    conflict

--- FCUBEAPP/src/app/app.module.ts
+++ FCUBEAPP/src/app/app.module.ts
@@ -459,10 +459,10 @@ import { GeneratetempgceditComponent } from './module/generatetempgc/generatetem
     TyrepurchaserptComponent, TyrestockrptComponent,
     TyrehistoryrptComponent, TyreactiverptComponent,
     TyreactivatedrptComponent, TyredeactivatedrptComponent,
-    TyreregroupissrptComponent, TyreregrouprcvdrptComponent, VehiclerepmaintaddComponent, VehiclerepmaintlistComponent, VehicleadvbalreceiptaddComponent, VehicleadvbalreceiptlistComponent
     TyreregroupissrptComponent, TyreregrouprcvdrptComponent, 
     VehiclerepmaintaddComponent, VehiclerepmaintlistComponent, 
-    DprplacevehicleComponent, GeneratetempgceditComponent
+    VehicleadvbalreceiptaddComponent, VehicleadvbalreceiptlistComponent,
+    DprplacevehicleComponent, GeneratetempgceditComponent,
   ],
   imports: [
     BrowserModule,
