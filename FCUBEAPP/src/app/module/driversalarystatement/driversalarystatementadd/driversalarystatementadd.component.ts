9a72ddfd704183c5f9e56aa0c0b9bccb842eba2a
Puttareddy-Suneetha <puttareddy.sunitha@meritinfo.com>
2024-09-04 14:55:47 +0530

    trip sheet

--- FCUBEAPP/src/app/module/driversalarystatement/driversalarystatementadd/driversalarystatementadd.component.ts
+++ FCUBEAPP/src/app/module/driversalarystatement/driversalarystatementadd/driversalarystatementadd.component.ts
@@ -186,14 +186,11 @@ exit(): void {
     get formArray() {
       return this.formDriverSalaryStatement.get("arrayList") as FormArray;
     }
+    
   getCreditAcList2(e:any){
-    this.ptype = e;
-    var data = {
-      'pType' : this.ptype
-    }
-  
+    this.requestmodel.strRequest = e;      
 
-    this.commonService.getCreditAcList2(data).subscribe((res) => {
+    this.commonService.getCreditAcList2(this.requestmodel).subscribe((res) => {
       this.creditacList = res;
       this.newList = this.creditacList;
     });
@@ -224,9 +221,9 @@ exit(): void {
     console.log(e.target.value);
     var selectedValue = e.target.value;
     this.ptype = e.target.value;
-    this.getCreditAcList2(this.ptype);
-  
+    this.getCreditAcList2(this.ptype);  
   }
+
   createInitialArray() {
     return this.formBuilder.group({
       vehicleNo:  ['', []],
