f9afffb5b8574e83f511adbce9dff53c2f387688
aashishwarenew <aashishware@meritinfo.com>
2024-09-06 10:51:47 +0530

    cust wizard

--- FCUBEAPI/FinTrans/Models/CustWizardModel.cs
+++ FCUBEAPI/FinTrans/Models/CustWizardModel.cs
@@ -8,33 +8,57 @@ namespace FinTrans.Models
 {
     public class CustWizardModel
     {
-        public string? Custwizid { get; set; }
+        public string? CustwizId { get; set; }
         public string? CashAc { get; set; }
-        public string? HsdAc { get; set; }
-        public string? TripRoutExpAc { get; set; }
-        public string? TripRepairsAc { get; set; }
-        public string?TripParkingAc { get; set; }
-        public string?TripMChallanAc { get; set; }
-        public string?TripWeighmentAc { get; set; }
-        public string?TripAccidentAc { get; set; }
-        public string?TripTollAc { get; set; }
-        public string?TripOthersMiscAc { get; set; }
-        public string?TripDrAlAc { get; set; }
-        public string?DelayDamageAc { get; set; }
-        public string?SgstInputAc { get; set; }
-        public string?CgstInputAc { get; set; }
-        public string?IgstInputAc{ get; set; }
-        public string?FastagTollAc{ get; set; }
-        public string?HappayAc { get; set; }
-        public string?DriverPoolAc { get; set; }
-        public string?DriverSalAc { get; set; }
-        public string?FrtIncAc { get; set; }
-        public string? TripTravelAlAc { get; set; }
-        public string? TripIncentiveAc { get; set; }
-        public string? TripRecdDrAc { get; set; }
-        public string? DslDiscAc { get; set; }
-        public string? DslTdsAc { get; set; }
-        public string? RoundOffAc { get; set; }
+        public string? FrtIncomeAc { get; set; }
+        public string? SgstOutputAc { get; set; }
+        public string? CgstOutputAc { get; set; }
+        public string? IgstOutputAc { get; set; }
+        public string? SgstInputAc { get; set; }
+        public string? CgstInputAc { get; set; }
+        public string? IgstInputAc { get; set; }
+        public string? LH_LorryHireAc { get; set; }
+        public string? LH_LorryHirePayableAc { get; set; }
+        public string? LH_TdsOnLorryHireAc { get; set; }
+        public string? LHP_HamaliAc { get; set; }
+        public string? LHP_DetentionAc { get; set; }
+        public string? LHP_OtherChargesAc { get; set; }
+        public string? LHP_LhpmAc { get; set; }
+        public string? LHP_RecoveryAc { get; set; }
+        public string? LHP_OthDedAc { get; set; }
+
+        public string? MR_FrtDeductionAc { get; set; }
+        public string? MR_ClaimsByPartyAc { get; set; }
+        public string? MR_BadDebtsAc { get; set; }
+        public string? MR_MiscDedAc { get; set; }
+        public string? MR_BankChargesAc { get; set; }
+        public string? MR_CashDiscAc { get; set; }
+        public string? MR_ExcessRecdAc { get; set; }
+        public string? MR_TdsDedAc { get; set; }
+        public string? MR_OthDedAc { get; set; }
+
+        public string? Flt_TyreStockAc { get; set; }
+        public string? Flt_TyreExpAc { get; set; }
+        public string? Flt_TyreSalesAc { get; set; }
+        public string? Flt_SparesStockAc { get; set; }
+        public string? Flt_LubesStockAc { get; set; }
+
+        public string? Flt_VehMa { get; set; }
+        public string? Flt_TripDrAdvanceAc { get; set; }
+        public string? Flt_TripFrtIncomeAc { get; set; }
+        public string? Flt_FltFrtReceivableAc { get; set; }
+        public string? Flt_TripExpensesAc { get; set; }
+        public string? Flt_DslPetroCardAc { get; set; }
+        public string? Flt_HappayCardAc { get; set; }
+        public string? Flt_TripDslExpAc { get; set; }
+        public string? Flt_TripAdblueExpAc { get; set; }
+        public string? Flt_DriverSalaryAc { get; set; }
+        public string? Flt_TripSuspenseAc { get; set; }
+        public string? Flt_ExtraChargesAc { get; set; }
+        public string? Flt_FrtDedAc { get; set; }
+        public string? Flt_TdsDedAc { get; set; }
+        public string? Flt_OthDedAc { get; set; }
+
 
 
     }
