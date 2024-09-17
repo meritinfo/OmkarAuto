0523ddc47f80cd25bcff9b6dc5ee928e9c598544
puttareddy.sunitha@meritinfo.com <puttareddy.sunitha@meritinfo.com>
2024-09-17 16:10:22 +0530

    tyre purchase feedback

--- FCUBEAPP/src/app/services/tyrepurchasemaster.service.ts
+++ FCUBEAPP/src/app/services/tyrepurchasemaster.service.ts
@@ -55,7 +55,4 @@ export class TyrePurchaseMasterService {
   getTyrePurchaseMasterList(filter: Filtermodel): Observable<Tyrepurchasemasterlistmodel> {
     return this.httpClient.post<Tyrepurchasemasterlistmodel>(Constants.API_ENDPOINT + 'FleetTrans/GetTyrePurchaseMasterList', filter, this.httpOptions);
   }  
-  getVendorDetails(filter: Requestmodel): Observable<Requestmodel> {
-    return this.httpClient.post<Requestmodel>(Constants.API_ENDPOINT + 'FleetTrans/GetVendorDetails', filter, this.httpOptions);
-  }  
 }
