9ad65501cc52bbf98f419d4852c8c19dc122a658
Puttareddy-Suneetha <puttareddy.sunitha@meritinfo.com>
2024-11-12 14:58:34 +0530

    transporter file download issue

--- FCUBEAPP/src/app/common/constants.ts
+++ FCUBEAPP/src/app/common/constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
- public static API_ENDPOINT = "http://localhost:65496/api/";
+public static API_ENDPOINT = "http://localhost:65496/api/";
 // public static API_ENDPOINT = "http://103.73.189.186/nccapi/api/";
-// public static API_ENDPOINT = "http://103.73.189.186/brplapi/api/";
+ //public static API_ENDPOINT = "http://103.73.189.186/brplapi/api/";
 
   //  public static API_ENDPOINT = "http://120.138.9.94/nccapi/api/";
 
