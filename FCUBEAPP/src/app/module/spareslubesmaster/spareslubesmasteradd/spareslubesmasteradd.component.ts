5425b8c528281fe31a56e6d64fcdf3a3b2bb06c7
Puttareddy-Suneetha <puttareddy.sunitha@meritinfo.com>
2024-12-25 17:33:41 +0530

    spare lub

--- FCUBEAPP/src/app/module/spareslubesmaster/spareslubesmasteradd/spareslubesmasteradd.component.ts
+++ FCUBEAPP/src/app/module/spareslubesmaster/spareslubesmasteradd/spareslubesmasteradd.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Spareslubesmasterlistmodel } from 'src/app/models/spareslubesmasterlistmodel';
 import { Spareslubesmastermodel } from 'src/app/models/sparelubesmastermodel';
 import { SparesLubesMasterService } from 'src/app/services/spareslubesmaster.service';
 import { Requestmodel } from 'src/app/models/requestmodel';
 import { Dropdownmodel } from 'src/app/models/dropdownmodel';
 import { Responsemodel } from 'src/app/models/responsemodel';
 import { CommonService } from 'src/app/services/common.service';
-
 import { ToastrService } from 'ngx-toastr';
 import { SharedService } from 'src/app/services/shared.service';
 
@@ -43,95 +40,68 @@ export class SpareslubesmasteraddComponent {
     private toasterService: ToastrService) {
     this.sparesLubesMasterModel = new Spareslubesmastermodel();
 
-}
-ngOnInit(): void {
-  var menuData = sessionStorage.getItem('menulist')?.toString();
-  if (typeof menuData !== 'undefined' && menuData !== null && menuData !== '') {
-    var privilegeData = JSON.parse(menuData);
-    var menuTypeList = privilegeData.flatMap((item: { menuTypeList: any; }) => item.menuTypeList);
-    var privilegeStatus = menuTypeList.flatMap((item: { menuList: any; }) => item.menuList)
-    .find((aa: { menuName: string; }) => aa.menuName === "Spares/Lubricants Master");
-    if (privilegeStatus) {
-      this.createStatus = privilegeStatus.createYN.toLowerCase() === "y" ? true : false;
-      this.editStatus = privilegeStatus.editYN.toLowerCase() === "y" ? true : false;
-      this.deleteStatus = privilegeStatus.deleteYN.toLowerCase() === "y" ? true : false;
-      this.viewStatus = privilegeStatus.viewYN.toLowerCase() === "y" ? true : false;
+  }
+  ngOnInit(): void {
+    var menuData = sessionStorage.getItem('menulist')?.toString();
+    if (typeof menuData !== 'undefined' && menuData !== null && menuData !== '') {
+      var privilegeData = JSON.parse(menuData);
+      var menuTypeList = privilegeData.flatMap((item: { menuTypeList: any; }) => item.menuTypeList);
+      var privilegeStatus = menuTypeList.flatMap((item: { menuList: any; }) => item.menuList)
+      .find((aa: { menuName: string; }) => aa.menuName === "Spares/Lubricants Master");
+      if (privilegeStatus) {
+        this.createStatus = privilegeStatus.createYN.toLowerCase() === "y" ? true : false;
+        this.editStatus = privilegeStatus.editYN.toLowerCase() === "y" ? true : false;
+        this.deleteStatus = privilegeStatus.deleteYN.toLowerCase() === "y" ? true : false;
+        this.viewStatus = privilegeStatus.viewYN.toLowerCase() === "y" ? true : false;
+      }
     }
+    
+    var userData = sessionStorage.getItem('uid')?.toString();
+    
+    if (typeof userData !== 'undefined' && userData !== null && userData !== '') {
+      this.loggedInUserID = userData;
+    }
+    if (this.loggedInUserID) {
+      console.log(this.loggedInUserID);
+    }
+    else {
+      this.route.navigate(['/']);
+    }
+
+    this.sharedService.loading = true;   
+    this.getBrandList();
+    this.selectedSpareslubesMasterDetails = this.sparesLubesMasterService.getSparesLubesMasterDetails();
+    this.formSparesMaster = this.formBuilder.group({   
+      spareLubName: new FormControl('',[Validators.required]),
+      spareLubType: new FormControl('',[Validators.required]),
+      sch_Oth: new FormControl('',[Validators.required]),
+      lifeType: new FormControl('',[Validators.required]),
+      lifeExpectancy: new FormControl('',[Validators.required]),
+      isActive: new FormControl('Y',[Validators.required]),
+      inventroyYN: new FormControl('',[Validators.required]),
+      arrayList: this.formBuilder.array([this.createInitialArray()])
+    });
+
+    if (this.selectedSpareslubesMasterDetails.spareLubId != '') {
+      this.formSparesMaster.patchValue(this.selectedSpareslubesMasterDetails); 
+      this.getSparesLubesInnerGridList();  
+      this.editMode = true;
+    }
+    
+    this.sharedService.loading = false;
+
   }
-  
-  var userData = sessionStorage.getItem('uid')?.toString();
-  
-  if (typeof userData !== 'undefined' && userData !== null && userData !== '') {
-    this.loggedInUserID = userData;
-  }
-  if (this.loggedInUserID) {
-    console.log(this.loggedInUserID);
-  }
-  else {
-    this.route.navigate(['/']);
+
+  get f() { return this.formSparesMaster.controls; }
+
+  get formArray() {
+    return this.formSparesMaster.get("arrayList") as FormArray;
   }
 
-  this.sharedService.loading = true;   
-  this.getBrandList();
-  this.selectedSpareslubesMasterDetails = this.sparesLubesMasterService.getSparesLubesMasterDetails();
-  this.formSparesMaster = this.formBuilder.group({   
-    spareLubName: new FormControl('',[Validators.required]),
-    spareLubType: new FormControl('',[Validators.required]),
-    sch_Oth: new FormControl('',[Validators.required]),
-    lifeType: new FormControl('',[Validators.required]),
-    lifeExpectancy: new FormControl('',[Validators.required]),
-    isActive: new FormControl('Y',[Validators.required]),
-    inventroyYN: new FormControl('',[Validators.required]),
-    arrayList: this.formBuilder.array([this.createInitialArray()])
-   // openingQty: new FormControl(''),
-   // openingValue: new FormControl(''),
-   
-  });
 
-  if (this.selectedSpareslubesMasterDetails.spareLubId != '') {
-    this.formSparesMaster.patchValue(this.selectedSpareslubesMasterDetails);  
-    this.formSparesMaster.patchValue({
-   
-    });    
-    this.getSparesLubesInnerGridList();  
-    this.editMode = true;
-  }
-  
-  this.sharedService.loading = false;
-
-}
-get f() { return this.formSparesMaster.controls; }
-get formArray() {
-  return this.formSparesMaster.get("arrayList") as FormArray;
-}
-//Get Classification List details //  
-
-
-// chkClassificationNameExits(e: any) { 
-//   if (this.selectedClassificationMasterDetails.centreid == "")
-//   {      
-//     this.sharedService.loading = true;
-//     this.requestmodel.strRequest = e.target.value; 
-//     this.classificationmasterService.chkClassificationNameExits(this.requestmodel).subscribe((res: Responsemodel) => {
-//       this.responseDetails = res;
-//       if (!this.responseDetails.status) {
-//         this.toasterService.warning(this.responseDetails.message);
-//         this.formClassificationMaster.patchValue({
-//           userClassification: ''
-//         });
-//       }
-//     });
-//     this.sharedService.loading = false;
-//   }
-// }
-
-
-chkSpareDuplicate(){
-  var selectedData = this.formSparesMaster.getRawValue();
-  
-
+  chkSpareDuplicate(){
+    var selectedData = this.formSparesMaster.getRawValue();
     this.requestmodel.strRequest = selectedData.spareLubName;
-  //  this.requestmodel.strRequest1 = selectedData.gcNoteNo;
     this.sparesLubesMasterService.checkDuplicateSpare(this.requestmodel).subscribe((res: Responsemodel) => {
       this.responseDetails = res;
       if (this.responseDetails.status) {
@@ -140,118 +110,108 @@ chkSpareDuplicate(){
       else{
         this.toasterService.warning(this.responseDetails.message);
         this.formSparesMaster.patchValue({
-          spareLubName: ''
-  
-        });
-        
+          spareLubName: ''  
+        });        
       }
+    });    
+  }
+
+  getBrandList(): void {
+    this.sparesLubesMasterService.getBrandList().subscribe((res) => {
+      this.brandList = res;
     });
-    
-}
-getBrandList(): void {
-  this.sparesLubesMasterService.getBrandList().subscribe((res) => {
-    this.brandList = res;
-  });
-}
-addItem(index: number): void {
-  if (this.formArray.value[index].brandId != "0" && this.formArray.value[index].openingQty != "") {
-    this.formArray.push(this.createInitialArray());
-  } else {
-    this.toasterService.warning("Please select one Item  detail brand, qty");
   }
-}
 
-removeItem(index: number) {
-  if (confirm("Are you sure, you want to delete this row?")) {
-    this.formArray.removeAt(index);
-  }
-}
-
-createInitialArray() {
-  return this.formBuilder.group({      
-    spareLubId: ['', []],
-    brandId: ['0', []],
-    openingQty: ['0', []],
-    openingValue: ['', []],
-  });
-}
-
-exit(): void {
-  this.route.navigate(['/sparesmasterlist']);
-}
-getSparesLubesInnerGridList(): void {
-  this.requestmodel.strRequest= this.selectedSpareslubesMasterDetails.spareLubId;
-  this.sparesLubesMasterService.getSparesLubesMasterInnerGridList(this.requestmodel).subscribe((res) => {
-    this.formArray.clear();
-    this.sparesLubesMasterModel = res;
-    for (var i = 0; i < res.sparesLubesDetailList.length; i++) {
+  addItem(index: number): void {
+    if (this.formArray.value[index].brandId != "0" && this.formArray.value[index].openingQty != "") {
       this.formArray.push(this.createInitialArray());
-     // this.formArray.controls[i].get("spareLubId")?.setValue(this.issueList.find(e => e.dataId == res.sparesLubesIssueDetailList[i].spareLubId));
-      this.formArray.controls[i].get("brandId")?.setValue(res.sparesLubesDetailList[i].brandId);
-      this.formArray.controls[i].get("openingQty")?.setValue(res.sparesLubesDetailList[i].openingQty);
-      this.formArray.controls[i].get("openingValue")?.setValue(res.sparesLubesDetailList[i].openingValue);       
-    }     
-  });
-}
-
-deleteSparesLubesMasterForm(): void {
-  if(this.selectedSpareslubesMasterDetails.spareLubId != '' ){
-   this.requestmodel.strRequest =this.selectedSpareslubesMasterDetails.spareLubId
-    if (confirm("Are you sure, you want to delete this?")) {
-          this.sparesLubesMasterService.sparesLubesMasterDelete(this.requestmodel).subscribe((res: Responsemodel) => {
-          this.responseDetails = res;
-          if (this.responseDetails.status) {
-            this.toasterService.success(this.responseDetails.message);
-            this.formSparesMaster.reset();
-            this.route.navigate(['/sparesmasterlist']);
-          }
-          else {
-            this.toasterService.warning(this.responseDetails.message);
-          }
-      });
+    } else {
+      this.toasterService.warning("Please select one Item  detail brand, qty");
     }
   }
-}
 
-
-//Submit user form details //
-submitSparesLubesMasterForm(): void {  
-  if (this.formSparesMaster.invalid) {
-    this.toasterService.warning("Please Enter Mandatory Fields ");
-    const controls = this.formSparesMaster.controls;
-    for (const name in controls) {
-      if (controls[name].invalid) {
-        this.toasterService.warning(name + " Fields is Invalid");   
-      }
-    } 
-    return;
+  removeItem(index: number) {
+    if (confirm("Are you sure, you want to delete this row?")) {
+      this.formArray.removeAt(index);
+    }
   }
-    
-  this.sharedService.loading = true;
 
-  var selectedDataVal = this.formSparesMaster.getRawValue();
-  this.formSubmitted = true;
-  this.sparesLubesMasterModel.spareLubId = this.selectedSpareslubesMasterDetails.spareLubId ;
-  this.sparesLubesMasterModel.spareLubName  = selectedDataVal.spareLubName.toString().toUpperCase();
-  this.sparesLubesMasterModel.spareLubType = selectedDataVal.spareLubType;
-  this.sparesLubesMasterModel.sch_Oth = selectedDataVal.sch_Oth;
-  this.sparesLubesMasterModel.lifeType = selectedDataVal.lifeType;
-  this.sparesLubesMasterModel.lifeExpectancy = selectedDataVal.lifeExpectancy;
-  this.sparesLubesMasterModel.isActive = selectedDataVal.isActive;
-  this.sparesLubesMasterModel.inventroyYN = selectedDataVal.inventroyYN;
-//  this.sparesLubesMasterModel.openingQty = selectedDataVal.openingQty;
- // this.sparesLubesMasterModel.openingValue = selectedDataVal.openingValue;
-  //this.classificationModel.loggedInUserID   = this.loggedInUserID;
-  this.sparesLubesMasterModel.sparesLubesDetailList = [];
+  createInitialArray() {
+    return this.formBuilder.group({      
+      spareLubId: ['', []],
+      brandId: ['0', []],
+      openingQty: ['0', []],
+      openingValue: ['', []],
+    });
+  }
+
+  getSparesLubesInnerGridList(): void {
+    this.requestmodel.strRequest= this.selectedSpareslubesMasterDetails.spareLubId;
+    this.sparesLubesMasterService.getSparesLubesMasterInnerGridList(this.requestmodel).subscribe((res) => {
+      if(res.sparesLubesDetailList.length>0){
+        this.formArray.clear();
+      }
+      this.sparesLubesMasterModel = res;
+      for (var i = 0; i < res.sparesLubesDetailList.length; i++) {
+        this.formArray.push(this.createInitialArray());
+        this.formArray.controls[i].get("brandId")?.setValue(res.sparesLubesDetailList[i].brandId);
+        this.formArray.controls[i].get("openingQty")?.setValue(res.sparesLubesDetailList[i].openingQty);
+        this.formArray.controls[i].get("openingValue")?.setValue(res.sparesLubesDetailList[i].openingValue);       
+      }     
+    });
+  }
+
+  exit(): void {
+    this.route.navigate(['/sparesmasterlist']);
+  }
+
+  deleteSparesLubesMasterForm(): void {
+    if(this.selectedSpareslubesMasterDetails.spareLubId != '' ){
+    this.requestmodel.strRequest =this.selectedSpareslubesMasterDetails.spareLubId
+      if (confirm("Are you sure, you want to delete this?")) {
+            this.sparesLubesMasterService.sparesLubesMasterDelete(this.requestmodel).subscribe((res: Responsemodel) => {
+            this.responseDetails = res;
+            if (this.responseDetails.status) {
+              this.toasterService.success(this.responseDetails.message);
+              this.formSparesMaster.reset();
+              this.route.navigate(['/sparesmasterlist']);
+            }
+            else {
+              this.toasterService.warning(this.responseDetails.message);
+            }
+        });
+      }
+    }
+  }
+
+  //Submit user form details //
+  submitSparesLubesMasterForm(): void {  
+    if (this.formSparesMaster.invalid) {
+      this.toasterService.warning("Please Enter Mandatory Fields ");
+      const controls = this.formSparesMaster.controls;
+      for (const name in controls) {
+        if (controls[name].invalid) {
+          this.toasterService.warning(name + " Fields is Invalid");   
+        }
+      } 
+      return;
+    }
+      
+    this.sharedService.loading = true;
+
+    var selectedDataVal = this.formSparesMaster.getRawValue();
+    this.formSubmitted = true;
+    this.sparesLubesMasterModel.spareLubId = this.selectedSpareslubesMasterDetails.spareLubId ;
+    this.sparesLubesMasterModel.spareLubName  = selectedDataVal.spareLubName.toString().toUpperCase();
+    this.sparesLubesMasterModel.spareLubType = selectedDataVal.spareLubType;
+    this.sparesLubesMasterModel.sch_Oth = selectedDataVal.sch_Oth;
+    this.sparesLubesMasterModel.lifeType = selectedDataVal.lifeType;
+    this.sparesLubesMasterModel.lifeExpectancy = selectedDataVal.lifeExpectancy;
+    this.sparesLubesMasterModel.isActive = selectedDataVal.isActive;
+    this.sparesLubesMasterModel.inventroyYN = selectedDataVal.inventroyYN;
+    this.sparesLubesMasterModel.sparesLubesDetailList = [];
+
     for (var i = 0; i < selectedDataVal.arrayList.length; i++) {  
-     // var spareLubId = this.issueList.find(e => e.dataId == selectedDataValue.arrayList[i].spareLubId.dataId) 
-     // if (typeof spareLubId !== 'undefined' && spareLubId !== null && spareLubId.dataId!="" && spareLubId.dataId!="0") {
-          //ignore
-      // }
-      // else{
-      //   this.toastrService.warning("Please Enter Valid Item Name");          
-      //   return;
-      // }
       if (selectedDataVal.arrayList[i].brandId =='0' ) {
         this.toasterService.warning("Please select Brand");
         return;
@@ -264,26 +224,23 @@ submitSparesLubesMasterForm(): void {
       this.sparesLubesMasterModel.sparesLubesDetailList.push({     
         'id': '',
         'spareLubId': '',
-       
         'brandId': selectedDataVal.arrayList[i].brandId,
         'openingQty': selectedDataVal.arrayList[i].openingQty,
         'openingValue': selectedDataVal.arrayList[i].openingValue
       })
-    }
-  
-    
+    }  
 
-  this.sparesLubesMasterService.sparesLubesMasterSubmitted(this.sparesLubesMasterModel).subscribe((res: Responsemodel) => {
-    this.responseDetails = res;
-    if (this.responseDetails.status) {
-      this.toasterService.success(this.responseDetails.message);
-      this.formSparesMaster.reset();
-      this.route.navigate(['/sparesmasterlist']);
-    }
-    else {
-      this.toasterService.warning(this.responseDetails.message);
-    }      
-  });
-  this.sharedService.loading = false;
-}
+    this.sparesLubesMasterService.sparesLubesMasterSubmitted(this.sparesLubesMasterModel).subscribe((res: Responsemodel) => {
+      this.responseDetails = res;
+      if (this.responseDetails.status) {
+        this.toasterService.success(this.responseDetails.message);
+        this.formSparesMaster.reset();
+        this.route.navigate(['/sparesmasterlist']);
+      }
+      else {
+        this.toasterService.warning(this.responseDetails.message);
+      }      
+    });
+    this.sharedService.loading = false;
+  }
 }
