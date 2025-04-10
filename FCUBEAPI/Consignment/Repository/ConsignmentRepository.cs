dd6ddfda5001e7dc7097ea7d3e5dcc627f3136a2
aashishwarenew <aashishware@meritinfo.com>
2025-04-10 15:16:18 +0530

    billsmaster

--- FCUBEAPI/Consignment/Repository/ConsignmentRepository.cs
+++ FCUBEAPI/Consignment/Repository/ConsignmentRepository.cs
@@ -778,6 +778,7 @@ namespace Consignment.Repository
                             new SqlParameter("@UlReportingDateTime",ConsignmentModel.UlReportingDateTime  ),
                             new SqlParameter("@DeliveryDateTime",   ConsignmentModel.DeliveryDateTime  ),
                             new SqlParameter("@UlDetentionDays",    ConsignmentModel.UlDetentionDays  ),
+                          //  new SqlParameter("@YearId",    ConsignmentModel.YearId),
                             new SqlParameter("@WhatsappPOD1",       ConsignmentModel.WhatsappPOD1   ),
                             new SqlParameter("@WhatsappPOD2",       ConsignmentModel.WhatsappPOD2   ),
                             new SqlParameter("@LoggedInUser",       ConsignmentModel.LoggedInUser),
