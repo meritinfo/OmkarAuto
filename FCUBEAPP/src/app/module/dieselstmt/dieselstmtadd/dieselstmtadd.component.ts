6024c8ecf7e08f6da18b15ff369da34839ffe2ee
aashishwarenew <aashishware@meritinfo.com>
2025-07-30 10:37:31 +0530

    tripshit llp fasttag grig edit mode issue

--- FCUBEAPP/src/app/module/dieselstmt/dieselstmtadd/dieselstmtadd.component.ts
+++ FCUBEAPP/src/app/module/dieselstmt/dieselstmtadd/dieselstmtadd.component.ts
@@ -218,7 +218,7 @@ export class DieselstmtaddComponent {
   }
 
   getAcountList(): void {    
-    this.requestmodel.strRequest="D"
+    this.requestmodel.strRequest="F"
     this.commonService.getAccountList(this.requestmodel).subscribe((res) => {
       this.accountList = res;
     });
--- FCUBEAPP/src/app/module/tripmasterllp/tripmasterllpadd/tripmasterllpadd.component.ts
+++ FCUBEAPP/src/app/module/tripmasterllp/tripmasterllpadd/tripmasterllpadd.component.ts
@@ -956,12 +956,15 @@ export class TripmasterllpaddComponent {
     }
     
     for (var i = 0; i < selectedDataValue.fasttagList.length; i++) {
+      if(selectedDataValue.fasttagList[i].ftAmount!=''){
       this.tripsheetmodel.fasttagList.push({
+        
         'detailID': selectedDataValue.fasttagList[i].detailID,
         'transDate': selectedDataValue.fasttagList[i].transDate,
         'ftAmount':  selectedDataValue.fasttagList[i].ftAmount,
         'remarks': selectedDataValue.fasttagList[i].remarks,
       })
+      }
     }
     
     for (var i = 0; i < selectedDataValue.drExpList.length; i++) {
