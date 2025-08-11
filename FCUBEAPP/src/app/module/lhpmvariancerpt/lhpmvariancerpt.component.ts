63f46a1e9ee8ea94567b76515aa6d931a692b806
sunitha <puttareddy.sunitha@meritinfo.com>
2025-08-11 10:50:43 +0530

    report

--- FCUBEAPP/src/app/module/lhpmvariancerpt/lhpmvariancerpt.component.ts
+++ FCUBEAPP/src/app/module/lhpmvariancerpt/lhpmvariancerpt.component.ts
@@ -273,15 +273,7 @@ dashboard: string ="";
       }
       var selectedDataVal=this.formFilter.getRawValue();
       
-          let frmdt = new Date(selectedDataVal.fromDate);
-    let todt = new Date(selectedDataVal.toDate);
-    let maxdt = new Date(this.loginDate);
-    let mindt = new Date(this.minDate);
-
-    if (maxdt<frmdt || frmdt<mindt || maxdt<todt || todt<mindt) {
-      this.toastrService.warning("From Date and To Date should be with in Fin Year");
-      return;
-    }
+        
       this.filter.fromDate    = selectedDataVal.fromDate;
       this.filter.toDate      = selectedDataVal.toDate;
       this.filter.filterStr   = selectedDataVal.branch;
