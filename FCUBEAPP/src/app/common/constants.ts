b7d5e3733d527a29c17ba796fab4521228e8763a
sunitha <puttareddy.sunitha@meritinfo.com>
2025-10-16 15:36:21 +0530

    local added

--- FCUBEAPP/src/app/common/constants.ts
+++ FCUBEAPP/src/app/common/constants.ts
@@ -1,5 +1,6 @@
 export class Constants {
-  public static API_ENDPOINT = "http://localhost:65496/api/";
+  //public static API_ENDPOINT = "http://localhost:65496/api/";
+   public static API_ENDPOINT = "/nccapi/api/";
 
  //public static API_ENDPOINT = "http://103.180.163.199/fcubeapi/api/";
  //public static API_ENDPOINT = "http://103.180.163.199/nccapi/api/";
