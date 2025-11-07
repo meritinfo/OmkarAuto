9425bc897d9caac9a094ac69ae8818780b49d3a2
sunitha <puttareddy.sunitha@meritinfo.com>
2025-11-07 17:01:42 +0530

    trippayment

--- FCUBEAPP/src/app/common/constants.ts
+++ FCUBEAPP/src/app/common/constants.ts
@@ -4,6 +4,7 @@ export class Constants {
    //public static API_ENDPOINT = "/gsrapi/api/";
 //  public static API_ENDPOINT = "/brplapi/api/";
 //  public static API_ENDPOINT = "/lalitaapi/api/";
+ // public static API_ENDPOINT = "/rafleetapi/api/";
 
  //public static API_ENDPOINT = "http://103.180.163.199/fcubeapi/api/"; 
  //   public static API_ENDPOINT = "http://183.82.144.101/nccapi/api/";
