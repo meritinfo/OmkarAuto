990d117905670326a85200d330371dfdc7df7a28
sunitha <puttareddy.sunitha@meritinfo.com>
2025-11-17 15:24:41 +0530

    heap issue

--- FCUBEAPP/src/app/common/constants.ts
+++ FCUBEAPP/src/app/common/constants.ts
@@ -2,9 +2,9 @@ export class Constants {
   //public static API_ENDPOINT = "http://localhost:65496/api/";
   // public static API_ENDPOINT = "/nccapi/api/";
    //public static API_ENDPOINT = "/gsrapi/api/";
-//  public static API_ENDPOINT = "/brplapi/api/";
+  public static API_ENDPOINT = "/brplapi/api/";
 //  public static API_ENDPOINT = "/lalitaapi/api/";
-  public static API_ENDPOINT = "/jrwapi/api/";
+ // public static API_ENDPOINT = "/jrwapi/api/";
 
  //public static API_ENDPOINT = "http://103.180.163.199/fcubeapi/api/"; 
  //   public static API_ENDPOINT = "http://183.82.144.101/nccapi/api/";
