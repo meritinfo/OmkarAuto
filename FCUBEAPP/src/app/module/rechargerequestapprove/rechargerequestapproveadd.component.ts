890b47e336c056e3b718e76aa0fc12d0195d6c88
GantaSantosh891 <ganta.santosh@meritinfo.com>
2025-11-20 10:41:22 +0530

    changes

--- FCUBEAPP/src/app/module/rechargerequestapprove/rechargerequestapproveadd.component.ts
+++ FCUBEAPP/src/app/module/rechargerequestapprove/rechargerequestapproveadd.component.ts
@@ -80,7 +80,7 @@ export class RechargerequestapproveaddComponent {
       var privilegeData = JSON.parse(menuData);
       var menuTypeList = privilegeData.flatMap((item: { menuTypeList: any; }) => item.menuTypeList);
       var privilegeStatus = menuTypeList.flatMap((item: { menuList: any; }) => item.menuList)
-      .find((( aa: { menuName: string; }) => aa.menuName === "Trip Sheet"));      
+      .find((( aa: { menuName: string; }) => aa.menuName === "Fleet Card Recharge Request"));      
       if (privilegeStatus) {
         this.createStatus = privilegeStatus.createYN.toLowerCase() === "y" ? true : false;
         this.editStatus = privilegeStatus.editYN.toLowerCase() === "y" ? true : false;
