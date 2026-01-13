9a02f7e64dff952fab53ca156c326ed3dfd60e19
sunitha <puttareddy.sunitha@meritinfo.com>
2026-01-13 16:35:29 +0530

    lalitha feedback

--- FCUBEAPI/FleetMasters/Repository/VehicleFltMasterRepository.cs
+++ FCUBEAPI/FleetMasters/Repository/VehicleFltMasterRepository.cs
@@ -813,11 +813,7 @@ namespace FleetMasters.Repository
                         new SqlParameter("@PageSize",   request.PageSize),
                         new SqlParameter("@SortColumn", request.SortColumn),
                         new SqlParameter("@SortOrder",  request.SortOrder),
-                        new SqlParameter("@Search",     request.Search),
-                        new SqlParameter("@FromDate",   request.FromDate),
-                        new SqlParameter("@ToDate",     request.ToDate),
-                        new SqlParameter("@LoginBranch",request.FilterStr),
-                        new SqlParameter("@YearId",     request.FilterStr1),
+                        new SqlParameter("@Search",     request.Search)
                     };
                     var dataSet = await SqlHelper.SqlHelper.ExecuteDatasetAsync(dbconnection.Value.DBConnection, "usp_getVehicleMasterExcel", param);
 
