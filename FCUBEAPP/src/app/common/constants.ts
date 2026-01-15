63218abe1721fb6db481e83c51d24a979401e207
sunitha <puttareddy.sunitha@meritinfo.com>
2026-01-15 11:36:04 +0530

    mr cash disc

--- FCUBEAPP/src/app/common/constants.ts
+++ FCUBEAPP/src/app/common/constants.ts
@@ -1,13 +1,14 @@
 export class Constants {
-  public static API_ENDPOINT = "http://localhost:65496/api/";
+ // public static API_ENDPOINT = "http://localhost:65496/api/";
   // public static API_ENDPOINT = "/brlapi/api/";
  //  public static API_ENDPOINT = "/nccapi/api/";
  // public static API_ENDPOINT = "/gsrapi/api/";
-  //public static API_ENDPOINT = "/brplapi/api/";
+ // public static API_ENDPOINT = "/brplapi/api/";
  // public static API_ENDPOINT = "/jrwapi/api/";
+ // public static API_ENDPOINT = "/rafleetapi/api/";
  // public static API_ENDPOINT = "/lalitaapi/api/";
   //public static API_ENDPOINT = "/fcubeapi/api/";
-  //public static API_ENDPOINT = "/velapi/api/";
+  public static API_ENDPOINT = "/velapi/api/";
 
  //public static API_ENDPOINT = "http://103.180.163.199/fcubeapi/api/"; 
    // public static API_ENDPOINT = "http://183.82.144.101/nccapi/api/";
