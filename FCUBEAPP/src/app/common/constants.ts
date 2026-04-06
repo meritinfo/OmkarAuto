0d38d6288b8a03be1b89d123ef435758f5ecf6cf
sunitha <puttareddy.sunitha@meritinfo.com>
2026-04-06 12:36:05 +0530

    gst

--- FCUBEAPP/src/app/common/constants.ts
+++ FCUBEAPP/src/app/common/constants.ts
@@ -1,12 +1,13 @@
 export class Constants {
-   //public static API_ENDPOINT = "http://localhost:65496/api/";
+   public static API_ENDPOINT = "http://localhost:65496/api/";
   // public static API_ENDPOINT = "/brlapi/api/";
   // public static API_ENDPOINT = "/nccapi/api/";
-//  public static API_ENDPOINT = "/gsrapi/api/";
+ // public static API_ENDPOINT = "/gsrapi/api/";
+ //  public static API_ENDPOINT = "/geoapi/api/";
  // public static API_ENDPOINT = "/brplapi/api/";
  // public static API_ENDPOINT = "/jrwapi/api/";
  // public static API_ENDPOINT = "/rafleetapi/api/";
-  public static API_ENDPOINT = "/lalitaapi/api/";
+ // public static API_ENDPOINT = "/lalitaapi/api/";
  // public static API_ENDPOINT = "/fcubeapi/api/";
   //public static API_ENDPOINT = "/velapi/api/"; 
 
@@ -19,6 +20,7 @@ export class Constants {
     // ng build --configuration production --base-href /ncc/ 
     // ng build --configuration production --base-href /brl/ 
     // ng build --configuration production --base-href /gsr/  
+    // ng build --configuration production --base-href /geo/  
     // ng build --configuration production --base-href /brpl/ 
     // ng build --configuration production --base-href /jrw/     
     // ng build --configuration production --base-href /lalita/    
