152edb80db7f071291b582484e2cdbb802c8a91d
sunitha <puttareddy.sunitha@meritinfo.com>
2026-04-15 16:42:02 +0530

    align

--- FCUBEAPP/src/app/module/challanmaster/challanmasteradd/challanmasteradd.component.ts
+++ FCUBEAPP/src/app/module/challanmaster/challanmasteradd/challanmasteradd.component.ts
@@ -908,13 +908,13 @@ export class ChallanmasteraddComponent {
     }
     if(selectedData.ownTruckYN)
     {
-       this.requestmodel.strRequest = selectedData.truckNo.toString().toUpperCase();
-       this.lrentryService.checkVehicleNo(this.requestmodel).subscribe((res: Responsemodel) => {
-       this.responseDetails = res;
+      this.requestmodel.strRequest = selectedData.truckNo.toString().toUpperCase();
+      this.lrentryService.checkVehicleNo(this.requestmodel).subscribe((res: Responsemodel) => {
+        this.responseDetails = res;
         if (this.responseDetails.status) {
           //ignore
         }
-       else{
+        else{
           this.toastrService.warning(this.responseDetails.message);
           this.formUser.patchValue({
             truckNo:"",
@@ -926,30 +926,30 @@ export class ChallanmasteraddComponent {
     {
       this.requestmodel.strRequest = selectedData.truckNo.toString().toUpperCase();
       this.lrentryService.checkTruckNo(this.requestmodel).subscribe((res: Responsemodel) => {
-      this.responseDetails = res;
-      if (this.responseDetails.status) {
-        this.truckMasterService.getTruckMstDetails(this.requestmodel).subscribe((res) => {
-        var chlDate = new Date(selectedData.challanDateTime);
-        var panValid = res.panValidYN=="Y"?"Y":"";
-        var aadharLinked = res.aadharLinkedYN=="Y"?"Y":"";
-        var permitDate = new Date(this.commonService.formatDate(res.nationalPermitDt));
-        var clrchlDate=chlDate.setHours(0, 0, 0, 0);
-        var clrpermitDate=permitDate.setHours(0, 0, 0, 0);
-        var permitValid = clrchlDate<clrpermitDate ? "Y" : "";
-        this.formUser.patchValue({
-        vehicleOwnerName : res.ownerName.toString(),
-        vehicleOwnerPanNo: res.panNo.toString(),
-        vehicleOwnerAdd1 : res.address1.toString(),
-        vehicleOwnerAdd2 : res.address2.toString(),
-        vehicleOwnerMblNo: res.mobileNo.toString(),
-        permitValid      : permitValid,
-        panValid         : panValid,
-        aadharLinked     : aadharLinked,
-        vehicleModel     : res.model.toString(),
-        engineNo         : res.engineNo.toString(),
-        chassisNo        : res.chasisNo.toString(),
-        });            
-        });
+        this.responseDetails = res;
+        if (this.responseDetails.status) {
+          this.truckMasterService.getTruckMstDetails(this.requestmodel).subscribe((res) => {
+            var chlDate = new Date(selectedData.challanDateTime);
+            var panValid = res.panValidYN=="Y"?"Y":"";
+            var aadharLinked = res.aadharLinkedYN=="Y"?"Y":"";
+            var permitDate = new Date(this.commonService.formatDate(res.nationalPermitDt));
+            var clrchlDate=chlDate.setHours(0, 0, 0, 0);
+            var clrpermitDate=permitDate.setHours(0, 0, 0, 0);
+            var permitValid = clrchlDate<clrpermitDate ? "Y" : "";
+            this.formUser.patchValue({
+              vehicleOwnerName : res.ownerName.toString(),
+              vehicleOwnerPanNo: res.panNo.toString(),
+              vehicleOwnerAdd1 : res.address1.toString(),
+              vehicleOwnerAdd2 : res.address2.toString(),
+              vehicleOwnerMblNo: res.mobileNo.toString(),
+              permitValid      : permitValid,
+              panValid         : panValid,
+              aadharLinked     : aadharLinked,
+              vehicleModel     : res.model.toString(),
+              engineNo         : res.engineNo.toString(),
+              chassisNo        : res.chasisNo.toString(),
+            });            
+          });
         }
         else{
           this.toastrService.warning(this.responseDetails.message);
@@ -960,18 +960,18 @@ export class ChallanmasteraddComponent {
       });
     }
     else if(this.vehicleApiDataYN=="Y")
-     {
+    {
       this.requestmodel.strRequest = selectedData.truckNo;
       this.requestmodel.strRequest1 = this.loggedInUserID;
       this.dprvehiplacedService.getVehicleDetails(this.requestmodel).subscribe((res) => {
-      this.formUser.patchValue({
-          vehicleOwnerName  : res.vehOwnerName.toString(),
-          vehicleOwnerPanNo: res.ownerPan.toString(),
-          vehicleOwnerAdd1 : res.vehAdd1.toString(),
-          vehicleOwnerAdd2 : res.vehAdd2.toString(),
-          vehicleOwnerMblNo: res.vehOwnerMobile.toString(),
-      });         
-    });
+        this.formUser.patchValue({
+            vehicleOwnerName  : res.vehOwnerName.toString(),
+            vehicleOwnerPanNo: res.ownerPan.toString(),
+            vehicleOwnerAdd1 : res.vehAdd1.toString(),
+            vehicleOwnerAdd2 : res.vehAdd2.toString(),
+            vehicleOwnerMblNo: res.vehOwnerMobile.toString(),
+        });         
+      });
     }
   }
    
