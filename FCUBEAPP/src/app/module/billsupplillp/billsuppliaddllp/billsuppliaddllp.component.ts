da1d931126dcc97235cc2ae0d29baf04f1273b13
sunitha <puttareddy.sunitha@meritinfo.com>
2026-04-22 12:09:38 +0530

    bill

--- FCUBEAPP/src/app/module/billsupplillp/billsuppliaddllp/billsuppliaddllp.component.ts
+++ FCUBEAPP/src/app/module/billsupplillp/billsuppliaddllp/billsuppliaddllp.component.ts
@@ -1142,15 +1142,15 @@ FreightTotal(i: number, e: any) {
       }   
     }
 
-    if(this.billsmastermodel.againstVehicleYN =='Y'){
-      if(selectedDataValue.totalFreight==frtamt ){
-        //ignore
-      }
-      else{
-        this.toasterService.warning(" Freight Amount is Invalid");   
-        return;
-      }
-    }    
+    // if(this.billsmastermodel.againstVehicleYN =='Y'){
+    //   if(selectedDataValue.totalFreight==frtamt ){
+    //     //ignore
+    //   }
+    //   else{
+    //     this.toasterService.warning(" Freight Amount is Invalid");   
+    //     return;
+    //   }
+    // }    
     
     this.billsMasterService.saveBillsMasterDetails(this.billsmastermodel).subscribe((res: Responsemodel) => {
       this.responseDetails = res;
